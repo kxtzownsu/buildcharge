a8eff7fa3fbb2bac24fa2eb061a4b14b7643e7cd
kxtzownsu <kxtzownsu@gmail.com>
2026-04-23 14:33:45 -0400

    feat: add config.mk generation
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- scripts/make/kconfig.mk
+++ scripts/make/kconfig.mk
@@ -2,10 +2,12 @@ KCONFIG_SCRIPT := bash $(PROJECT_DIR)/scripts/kconfig/kconfig.sh
 KCONFIG_FILE   := $(WORK_DIR)/Kconfig
 DOT_CONFIG     := $(PROJECT_DIR)/.config
 CONFIG_SH      := $(PROJECT_DIR)/scripts/lib/generated/config.sh
+CONFIG_MK      := $(PROJECT_DIR)/scripts/lib/generated/config.mk
 
 KCONFIG_FLAGS  := --kconfig $(KCONFIG_FILE) \
                   --dot-config $(DOT_CONFIG) \
                   --config-sh $(CONFIG_SH) \
+									--config-mk $(CONFIG_MK) \
                   --manifest $(PROJECT_DIR)/ramfs/manifest.json
 
 .PHONY: menuconfig guiconfig gen-kconfig gen-config olddefconfig config
@@ -27,7 +29,9 @@ gen-kconfig:
 	$(Q)$(KCONFIG_SCRIPT) gen-kconfig --manifest $(PROJECT_DIR)/ramfs/manifest.json --kconfig $(KCONFIG_FILE)
 
 gen-config:
-	$(Q)$(KCONFIG_SCRIPT) gen-config --dot-config $(DOT_CONFIG) --config-sh $(CONFIG_SH)
+	$(Q)$(KCONFIG_SCRIPT) gen-config --dot-config $(DOT_CONFIG) \
+		--config-sh $(CONFIG_SH) \
+		--config-mk $(CONFIG_MK)
 
 olddefconfig:
 	$(Q)$(KCONFIG_SCRIPT) olddefconfig $(KCONFIG_FLAGS)
